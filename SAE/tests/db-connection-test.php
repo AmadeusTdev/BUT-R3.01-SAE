4bfdc5b0ce3c6e6ebca0e5d55e2599568be18c1a
eabc2318 <mathias.m2502@gmail.com>
2026-09-21 11:21:24 +0200

    renommage tu test et ajout de l'instance inscription

---
+++ SAE/tests/db-connection-test.php
@@ -0,0 +1,21 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../noyau/model.php';
+require_once __DIR__ . '/../modules/models/authentificationModel.php';
+require_once __DIR__ . '/../modules/models/inscriptionModel.php';
+
+try {
+    Model::checkConnection();
+    echo "Connexion réussie via Model\n";
+
+    AuthentificationModel::checkConnection();
+    echo "Connexion réussie via AuthentificationModel\n";
+
+    InscriptionModel::checkConnection();
+    echo "Connexion réussie via AuthentificationModel\n";
+} catch (PDOException $e) {
+    echo "Échec de connexion : " . $e->getMessage();
+}
