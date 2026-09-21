3320c4bfe5dca846d34d365548bdb74533018986
eabc2318 <mathias.m2502@gmail.com>
2026-09-21 11:22:31 +0200

    FEAT: ajout du model d'inscription

---
+++ SAE/modules/models/inscriptionModel.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . '/../../noyau/model.php';
+
+class InscriptionModel extends Model {
+    // Héritage de la classe paret du super model noyau
+
+    public static function register(
+        string $login,
+        string $firstName,
+        string $lastName,
+        string $email,
+        ?string $phoneNumber,
+        string $adress): bool {
+
+            $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+
+
+        
+        $sql = "INSERT INTO users (login, first_name, last_name, email, hash_password, phone_number, adress)
+                VALUES (:login, :first_name, :last_name, :email, :hash_password, :phone_number, :adress)";
+
+        $stmt = self::getPdo()->prepare($sql);
+
+        return $stmt->execute([
+            ':login'         => $login,
+            ':first_name'    => $firstName,
+            ':last_name'     => $lastName,
+            ':email'         => $email,
+            ':hash_password' => $hashPassword,
+            ':phone_number'  => $phoneNumber,
+            ':adress'        => $adress,
+        ]);
+    }
+}
+        
+        
